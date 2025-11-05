30686e98886b733b75d0926698d3b1508a7372f2
kishanshukla-2307 <2307kishanshukla@gmail.com>
2025-11-05 16:32:58 +0530

    windowed store implemented

--- java-deduper/src/main/java/com/example/dedup/DedupStreamApp.java
+++ java-deduper/src/main/java/com/example/dedup/DedupStreamApp.java
@@ -4,7 +4,9 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.*;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.state.StoreBuilder;
 import org.apache.kafka.streams.state.Stores;
+import org.apache.kafka.streams.state.WindowStore;
 
 import java.time.Duration;
 import java.util.Properties;
@@ -14,7 +16,9 @@ public class DedupStreamApp {
         final String bootstrap  = args.length>0?args[0]:"localhost:9092";
         final String inputTopic = args.length>1?args[1]:"input";
         final String outputTopic= args.length>2?args[2]:"output";
-        final String appId      = args.length>3?args[3]:"kstreams-dedup";
+        final String appId      = "kstreams-dedup";
+        final String storeName  = "seen-store";
+        final long minSpaceBtwEvents = 10;
 
         Properties p = new Properties();
         p.put(StreamsConfig.APPLICATION_ID_CONFIG, appId);
@@ -24,14 +28,28 @@ public class DedupStreamApp {
         p.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 200);
 
         StreamsBuilder b = new StreamsBuilder();
-        var store = Stores.keyValueStoreBuilder(
-            Stores.inMemoryKeyValueStore("seen-store"),
-            Serdes.String(), Serdes.Long()
-        );
-        b.addStateStore(store);
+        // var store = Stores.keyValueStoreBuilder(
+        //     Stores.inMemoryKeyValueStore("seen-store"),
+        //     Serdes.String(), Serdes.Long()
+        // );
+        // b.addStateStore(store);
+
+        final Duration windowSize = Duration.ofMinutes(10);
+        final Duration retentionPeriod = windowSize;
+
+        final StoreBuilder<WindowStore<String, Long>> dedupStoreBuilder = Stores.windowStoreBuilder(
+            Stores.persistentWindowStore(storeName,
+                                        retentionPeriod,
+                                        windowSize,
+                                        false
+            ),
+            Serdes.String(),
+            Serdes.Long());
+
+        b.addStateStore(dedupStoreBuilder);
 
         b.stream(inputTopic, Consumed.with(Serdes.String(), Serdes.String()))
-         .transform(() -> new DedupTransformer("seen-store"), "seen-store")
+         .transform(() -> new DedupTransformer(storeName, minSpaceBtwEvents), storeName)
          .to(outputTopic, Produced.with(Serdes.String(), Serdes.String()));
 
         KafkaStreams s = new KafkaStreams(b.build(), p);
--- java-deduper/src/main/java/com/example/dedup/DedupTransformer.java
+++ java-deduper/src/main/java/com/example/dedup/DedupTransformer.java
@@ -3,23 +3,46 @@ package com.example.dedup;
 import org.apache.kafka.streams.kstream.Transformer;
 import org.apache.kafka.streams.processor.ProcessorContext;
 import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.state.StoreBuilder;
+import org.apache.kafka.streams.state.WindowStore;
+import org.apache.kafka.streams.state.WindowStoreIterator;
 import org.apache.kafka.streams.KeyValue;
 
 public class DedupTransformer implements Transformer<String, String, KeyValue<String, String>> {
+    private ProcessorContext ctx;
     private final String storeName;
-    private KeyValueStore<String, Long> seen;
+    private WindowStore<String, Long> seen;
+    private final long minSpaceBtwEvents;
 
-    public DedupTransformer(String storeName) { this.storeName = storeName; }
 
-    @Override public void init(ProcessorContext context) { this.seen = context.getStateStore(storeName); }
+
+    public DedupTransformer(String storeName, long minSpaceBtwEvents) { 
+        this.storeName = storeName; 
+        this.minSpaceBtwEvents = minSpaceBtwEvents;
+    }
+
+    @Override public void init(ProcessorContext context) { 
+        this.ctx = context;
+        this.seen = ctx.getStateStore(storeName); 
+    }
 
     @Override public KeyValue<String, String> transform(String key, String value) {
         if (key == null) return null;
-        if (seen.get(key) == null) {
-            seen.put(key, System.currentTimeMillis());
+        Long eventTime = ctx.timestamp();
+
+        final WindowStoreIterator<Long> iterator = seen.fetch(
+            key, 
+            eventTime - minSpaceBtwEvents, 
+            eventTime + minSpaceBtwEvents);
+
+        // update the latest timestamp for the event
+        seen.put(key, eventTime, eventTime);
+        if (iterator.hasNext()) {
+            // already seen, do nothing
+            return null;
+        } else {
             return new KeyValue<>(key, value);
         }
-        return null; // drop duplicate
     }
     @Override public void close() {}
 }
